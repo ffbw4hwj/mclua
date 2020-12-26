ed4b632189e0aa1292a784560fc64be80f8361e1
ffbw4hwj <31228280+ffbw4hwj@users.noreply.github.com>
2020-12-26 04:03:24 -0500

    Update cstuff.lua

--- cstuff.lua
+++ cstuff.lua
@@ -10,17 +10,24 @@ end
 while (true) do
 
 	local slotnum = 1
-	while slotnum < 15 do
+	while slotnum < 12 do
 		print('clearing slot ' .. slotnum)
+		turtle.select(slotnum)
 		turtle.drop();
 		
 		-- grab stuff to 3x3 in inventory
 		if slotnum % 4 ~= 0 then
 			print('attempt to suck to slot ' .. slotnum)
-			turtle.select(slotnum)
-			while not turtle.suck() do
-				print('[!] front chest is empty, sleep 3 sec to try again')
-				os.sleep(3)
+			full_stack = false
+			repeat
+				turtle.drop()
+				non_empty = turtle.suck()
+				item_count = turtle.getItemCount()
+				full_stack = non_empty and item_count == 64
+				if not full_stack then
+					print('front chest lacking full stack, sleep 3 sec to try again')
+					os.sleep(3)
+				end
 			end
 		end
 		slotnum = slotnum + 1
